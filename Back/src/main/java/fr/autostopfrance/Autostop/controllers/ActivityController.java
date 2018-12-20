94c3663cf87cf65c0301b226f5d70d7332db3b5c
Eliott R <eliott.raillere@gmail.com>
2018-12-20 15:42:00 +0100

    pied

--- Back/src/main/java/fr/autostopfrance/Autostop/controllers/ActivityController.java
+++ Back/src/main/java/fr/autostopfrance/Autostop/controllers/ActivityController.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,12 @@ public class ActivityController {
 	@GetMapping("/{id}")
 	public Activity findActivity(@PathVariable("id") Long id) {
 		return activityService.findById(id);
+	}
+	
+	@PostMapping("/create")
+	public Activity addActivity(@RequestBody Activity activity) {
+		System.out.println("added activity");
+		Activity activity1 = activityService.postActivity(activity);
+		return activity1;
 	}
 }
--- Back/src/main/java/fr/autostopfrance/Autostop/models/User.java
+++ Back/src/main/java/fr/autostopfrance/Autostop/models/User.java
@@ -1,114 +1,44 @@
 package fr.autostopfrance.Autostop.models;
 
-import java.time.LocalDate;
 import javax.persistence.*;
 
 @Entity
 @Table(name="user")
 public class User {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userId")
+    @GeneratedValue
+    @Column(name="id")
     private Long id;
-    @Column(name="last_name")
-    private String lastName;
-    @Column(name="first_name")
-    private String firstName;
-    @Column(name="phone")
-    private String phone;
-    @Column(name="img_url")
-    private String imgUrl;
-    @Column(name="sex")
-    private String sex;
-    @Column(name="date_of_birth")
-    private LocalDate dateOfBirth;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private Account account;
-
-
-
-    public User () {}
-
-    public User (String lastName, String firstName, String phone, String sex, LocalDate dateOfBirth, String email, String password) {
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.phone = phone;
-        this.sex = sex;
-        this.dateOfBirth = dateOfBirth;
-        this.account = new Account(email, password);
+    protected User () {
     }
 
-    public User (String lastName, String firstName, String phone, String sex, LocalDate dateOfBirth, String email, String password, String fileDownloadUri) {
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.phone = phone;
-//        this.imgUrl = imgUrl;
-        this.sex = sex;
-        this.dateOfBirth = dateOfBirth;
-        this.account = new Account(email, password);
+    public User (String email, String password) {
+        this.email = email;
+        this.password = password;
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getEmail() {
+        return email;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getPassword() {
+        return password;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setPassword(String password) {
+        this.password = password;
     }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    @Override
-    public String toString() {
-        return " User: " + id +" " + firstName + " " + lastName + " " + dateOfBirth + " " + sex + " " + phone + " " + imgUrl + " !";
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
 }
