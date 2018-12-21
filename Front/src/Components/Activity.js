f120e57ba6a01c5671adc51491061a846786176d
Loïc Marchand <nilpa018@yahoo.fr>
2018-12-21 13:53:44 +0100

    ok

--- Front/src/Components/Activity.js
+++ Front/src/Components/Activity.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 import "../Styles/activity.css";
 import NavBar from "./Navbar";
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 import pictoreunion from "../images/picto_reunion.png";
+//import axios from 'axios';
 
 class Activity extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      sentenceNbr: 0
+    };
   }
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  getActivities = () => {
+    let numberActivities = this.getRandomInt(0,4);
+    axios("http://localhost:8080/activities/"+ numberActivities).then(res => {
+      const sentenceNum = res.data[0].sentence;
+      //console.log(candy1)
+      this.setState({
+        sentenceNbr: sentenceNum
+      });
+    });
+  };
+  componentWillMount = () => {
+    this.getActivities();
+}
 
   render() {
     return (
@@ -30,7 +51,9 @@ class Activity extends Component {
                 <button className="btn-nxt-orange">NEXT</button>
               </div>
             </div>
-            <NavLink to="/countdown"><button className="btn-go-orange">GO</button></NavLink>
+            <NavLink to="/countdown">
+              <button className="btn-go-orange">GO</button>
+            </NavLink>
           </div>
         </div>
       </div>
